d4c9319471f97722a3eae3458b75bfe94e7488d5
Sergey Lem <sergey.lem@gmail.com>
2020-12-03 23:21:01 +0600

    Added user reducer and user actions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,12 +6,12 @@ const User = require('../models/userModel')
 // @route   POST /api/user/login
 // @access  Public
 exports.authUser = asyncHandler(async (req, res) => {
-  
-  const {email, password} = req.body
 
-  const user = await User.findOne({email})
+  const { email, password } = req.body
 
-  if(user && (await user.matchPassword(password))) {
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -25,14 +25,48 @@ exports.authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Register a new user
+// @route   POST /api/user
+// @access  Public
+exports.registerUser = asyncHandler(async (req, res) => {
+
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('User not found')
+  }
+})
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 exports.getUserProfile = asyncHandler(async (req, res) => {
-  
+
   const user = await User.findById(req.user._id)
 
-  if(user) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,

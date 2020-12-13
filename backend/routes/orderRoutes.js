ed79164d951141ae2d1484fef30fe7f05dcbdac4
Sergey Lem <sergey.lem@gmail.com>
2020-12-13 21:58:48 +0600

    Added updating order

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const router = express.Router()
 
-const { addOrderItems } = require('../controllers/orderController')
+const { addOrderItems, getOrderById, updateOrderToPaid } = require('../controllers/orderController')
 
 const protect = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, addOrderItems)
+
+router.route('/:id').get(protect, getOrderById)
+
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 
 module.exports = router
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import {orderCreateReducer} from './reducers/orderReducers'
+import {orderCreateReducer, orderDetailsReducer, orderPayReducer} from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -19,7 +19,9 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-  orderCreate: orderCreateReducer
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay : orderPayReducer
 
 })
 
